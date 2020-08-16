540e57050e672b062492697e93b95398405dc43e
TrinhHiep <dinhhiep7793@gmail.com>
2020-08-16 09:54:07 +0700

    change4

--- src/Component/CardImg.jsx
+++ src/Component/CardImg.jsx
@@ -1,73 +1,14 @@
 import React from 'react'
 
-const CardImg = () => {
+const CardImg = (props) => {
     return (
-        <div className='container text-center'>
-            <div className='row'>
-                <div className='col-sm-4 mb-4'>
-                    <div className="card" style={{ width: '22rem' }}>
-                        <img src="https://i1.wp.com/4.bp.blogspot.com/-COTipEtZivY/Xyqtnnuf0dI/AAAAAAABcCk/sum6AWRy35E-GyyCcVXKLIUK9oIVH_xIQCLcBGAsYHQ/s0/Coser-Momoko-MrCong.com-005.jpg?w=955&ssl=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='col-sm-4 mb-4'>
-                    <div className="card" style={{ width: '22rem' }}>
-                        <img src="https://i0.wp.com/3.bp.blogspot.com/-z40h8YQakbQ/XyqtmRwSyGI/AAAAAAABcCY/qfOhtpUR_Bkkr4hZKD9Jib2whlqaHtbWACLcBGAsYHQ/s0/Coser-Momoko-MrCong.com-002.jpg?w=955&ssl=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='col-sm-4 mb-4'>
-                    <div className="card" style={{ width: '22rem' }}>
-                        <img src="https://i2.wp.com/2.bp.blogspot.com/-4UYOx7OQoJ8/Xy1XvVWvSRI/AAAAAAABeLk/OPb8bUlg6Dk-Yoa44BVucX5Izy8yT3PHgCLcBGAsYHQ/s1600/Coser-Momoko-MrCong.com-005.jpg?w=955&ssl=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='col-sm-4 mb-4'>
-                    <div className="card" style={{ width: '22rem' }}>
-                        <img src="https://i1.wp.com/1.bp.blogspot.com/-MHMBE_zOa0E/Xyv0XbUZjzI/AAAAAAABdTM/R08NuPHNPlIQO4EBQHMJ_cg6jyj250QZQCLcBGAsYHQ/s1600/Coser-Momoko-MrCong.com-001.jpg?w=955&ssl=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='col-sm-4 mb-4'>
-                    <div className="card" style={{ width: '22rem' }}>
-                        <img src="https://i0.wp.com/2.bp.blogspot.com/-him-92T7JnU/Xyv0XMc7FaI/AAAAAAABdTE/-7h9uDhQ1Kc-D-PsUCZbL1Dnz-_uu2kWACLcBGAsYHQ/s1600/Coser-Momoko-MrCong.com-003.jpg?w=955&ssl=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='col-sm-4 mb-4'>
-                    <div className="card" style={{ width: '22rem' }}>
-                        <img src="https://i1.wp.com/1.bp.blogspot.com/-J-IXernhjdQ/XyqtoeO3-GI/AAAAAAABcCs/72oUfLB67L0PlSLZG9oTEsqpjODnjKDuQCLcBGAsYHQ/s0/Coser-Momoko-MrCong.com-007.jpg?w=955&ssl=1" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
+        <div className='col-sm-4 mb-4'>
+            <div className="card" style={{ width: '22rem' }}>
+                <img src={props.dataImg} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="/" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,35 @@ import BtFooter from './Component/BtFooter'
 import Contact from './Component/Contact'
 import Footer from './Component/Footer'
 class Main extends Component {
-    state = {}
+    state = {
+        data: [
+            { link: "https://i1.wp.com/4.bp.blogspot.com/-COTipEtZivY/Xyqtnnuf0dI/AAAAAAABcCk/sum6AWRy35E-GyyCcVXKLIUK9oIVH_xIQCLcBGAsYHQ/s0/Coser-Momoko-MrCong.com-005.jpg?w=955&ssl=1" },
+            { link: "https://i0.wp.com/3.bp.blogspot.com/-z40h8YQakbQ/XyqtmRwSyGI/AAAAAAABcCY/qfOhtpUR_Bkkr4hZKD9Jib2whlqaHtbWACLcBGAsYHQ/s0/Coser-Momoko-MrCong.com-002.jpg?w=955&ssl=1" },
+            { link: "https://i2.wp.com/2.bp.blogspot.com/-4UYOx7OQoJ8/Xy1XvVWvSRI/AAAAAAABeLk/OPb8bUlg6Dk-Yoa44BVucX5Izy8yT3PHgCLcBGAsYHQ/s1600/Coser-Momoko-MrCong.com-005.jpg?w=955&ssl=1" },
+            { link: "https://i1.wp.com/1.bp.blogspot.com/-MHMBE_zOa0E/Xyv0XbUZjzI/AAAAAAABdTM/R08NuPHNPlIQO4EBQHMJ_cg6jyj250QZQCLcBGAsYHQ/s1600/Coser-Momoko-MrCong.com-001.jpg?w=955&ssl=1" },
+            { link: "https://i0.wp.com/2.bp.blogspot.com/-him-92T7JnU/Xyv0XMc7FaI/AAAAAAABdTE/-7h9uDhQ1Kc-D-PsUCZbL1Dnz-_uu2kWACLcBGAsYHQ/s1600/Coser-Momoko-MrCong.com-003.jpg?w=955&ssl=1" },
+            { link: "https://i1.wp.com/1.bp.blogspot.com/-J-IXernhjdQ/XyqtoeO3-GI/AAAAAAABcCs/72oUfLB67L0PlSLZG9oTEsqpjODnjKDuQCLcBGAsYHQ/s0/Coser-Momoko-MrCong.com-007.jpg?w=955&ssl=1" }
+        ]
+    }
     render() {
         return (
             <div>
                 <Navbar />
                 <Bg />
                 <NavMid />
-                <CardImg />
+                <div className='container text-center'>
+                    <div className='row'>
+                        {this.state.data.map(e => (
+                            <CardImg dataImg={e.link} />
+                        ))}
+                    </div>
+                </div>
                 <Pagi />
                 <BtFooter />
                 <hr />
                 <Contact />
                 <Footer />
-            </div>
+            </div >
         );
     }
 }
